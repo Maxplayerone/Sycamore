0216e168d7e43067ac602ac07d6bbe58818dc994
maxplayerone <antekmajner@gmail.com>
2022-07-27 10:01:39 +0200

    Added build system with premake

---
+++ premake5.lua
@@ -0,0 +1,15 @@
+--how the solution file will be named
+workspace "Sycamore"
+    architecture "x64"
+    configurations {"Debug", "Release"}
+
+    --setting up the startup project (only in visual studio)
+    startproject "Sycamore"
+
+--cool macros for output file
+outputdir = "%{cfg.system}/%{cfg.buildcfg}-%{cfg.architecture}"
+
+--check for the premake5.lua file in the given path
+--and executes the program
+include "Sycamore"
+
